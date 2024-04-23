f88db1c562a481314d543e55a3bd36cabab9507b
tgnlex <alexanderwebber76@gmail.com>
2024-04-23 05:10:14 -0700

    add tests

--- test/base.test.js
+++ test/base.test.js
@@ -1,7 +1,16 @@
 import {describe, expect, test} from '@jest/globals';
-import {add} from '../src/lib/base.js';
-
+import {add, json, log, read, noop, sleep, sleepMs} from '../src/lib/base.js';
+  
+  test('test read returns value', () => {
+    expect(read("hello world")).toBe("hello world")
+  });
+  
+  test('tests log returns properly logged value', () => {
+    expect(log("test", "hello world")).toBe(console.log("[TEST]", "hello world "));
+  })
 
   test('adds 3 + 4 to equal 7', () => {
     expect(add(3, 4)).toBe(7);
   });
+
+  
