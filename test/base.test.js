ff9421906c58296f8002753b662c384ed356cf7f
tgnlex <alexanderwebber76@gmail.com>
2024-04-23 05:43:30 -0700

    added genElement test

--- test/base.test.js
+++ test/base.test.js
@@ -1,38 +1,41 @@
-import {jest, describe, expect, test} from '@jest/globals';
-import {add, nullop, json, log, read, noop, sleep, sleepMs} from '../src/lib/base.js';
+import {jest, expect, test} from '@jest/globals';
+import {add, nullop, json, log, read, noop, sleep, sleepMs, genElement} from '../src/lib/base.js';
 jest.useFakeTimers();
 jest.spyOn(global, 'setTimeout');
 
 test("test that noop returns undefined", () => {
-    expect(noop()).toBe(undefined);
-  })  
-  test("test that nullop returns null", () => {
-    expect(nullop()).toBe(null);
-  })
-  test("test that sleep will setTimeout in seconds", () => {
-    sleep(1)
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
-  });
-  test('test that sleepMs will setTimeout in milliseconds', () => {
-    sleepMs(1000);
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000)
-  });
-  test('test that json returns a json string', () => {
-    const data = {test: "item"}
-    expect(json(data)).toBe('{\"test\":\"item\"}')
-  })
-  test('test read returns value', () => {
-    expect(read("hello world")).toBe("hello world")
-  });
+  expect(noop()).toBe(undefined);
+})  
+test("test that nullop returns null", () => {
+  expect(nullop()).toBe(null);
+})
+test("test that sleep will setTimeout in seconds", () => {
+  sleep(1)
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+});
+test('test that sleepMs will setTimeout in milliseconds', () => {
+  sleepMs(1000);
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+  expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000)
+});
+test('test that json returns a json string', () => {
+  const data = {test: "item"}
+  expect(json(data)).toBe('{\"test\":\"item\"}')
+})
+test('test read returns value', () => {
+  expect(read("hello world")).toBe("hello world")
+});
   
-  test('tests log returns properly logged value', () => {
-    expect(log("test", "hello world")).toBe(console.log("[TEST]", "hello world "));
-  })
+test('tests log returns properly logged value', () => {
+  expect(log("test", "hello world")).toBe(console.log("[TEST]", "hello world "));
+})
 
-  test('adds 3 + 4 to equal 7', () => {
-    expect(add(3, 4)).toBe(7);
-  });
-
+test('test that add(3 + 4) returns 7', () => {
+  expect(add(3, 4)).toBe(7);
+});
+test('test that genElement("p", "test") returns <p>test</p>, etc...', () => {
+  expect(genElement('p', 'test')).toBe('<p>test</p>');
+  expect(genElement('h1', 'test2')).toBe('<h1>test2</h1>');
+})
   
